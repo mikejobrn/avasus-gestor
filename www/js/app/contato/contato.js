011900f23ed9c6f225d460582a874f4afb86c718
Mike Job <mikejobrn@gmail.com>
2016-03-18 10:48:34 -0300

    Adiciona página de contato

---
+++ www/js/app/contato/contato.js
@@ -0,0 +1,33 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('AvasusGestor')
+        .controller('ContatoCtrl', ContatoCtrl);
+
+    ContatoCtrl.$inject = ['$ionicPopup', '$state'];
+
+    /* @ngInject */
+    function ContatoCtrl($ionicPopup, $state) {
+        var vm = this;
+
+        activate();
+
+        vm.enviar = function(contato) {
+            if (!contato ||
+                    !contato.nome || contato.nome.trim() === "" ||
+                    !contato.email || contato.email.trim() === "" ||
+                    !contato.mensagem || contato.mensagem.trim() === "") {
+
+                $ionicPopup.alert({ title: "Por favor, preencha todos os campos." });
+            } else {
+                $ionicPopup.alert({ title: "Sua mensagem foi enviada com sucesso!" });
+                $state.go("app.dash");
+            }
+        };
+
+        function activate() {
+
+        }
+    }
+})();
