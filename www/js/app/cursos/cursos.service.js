be1590231959d1307e2f48da181aa69e847fcaec
Mike Job <mikejobrn@gmail.com>
2016-03-18 20:49:38 -0300

    Ajusta filtro de Estados para Gráfico de Usuários por Curso

--- www/js/app/cursos/cursos.service.js
+++ www/js/app/cursos/cursos.service.js
@@ -10,10 +10,27 @@
     /* @ngInject */
     function cursoService(avasusService, $http) {
         var service = {
+            getDetalhes: getDetalhes,
             getResumoPorEstado: getResumoPorEstado
         };
 
         return service;
+
+        function getDetalhes(filtro) {
+            var url = avasusService.getUrl('widesus_dashboard_curso');
+
+            if (filtro && filtro.valor) {
+                url += '&' + filtro.campo + '=' + filtro.valor;
+            }
+
+            console.log(url);
+
+            return $http.get(url).then(
+                function (resultado) {
+                    return resultado.data;
+                }
+            );
+        }
 
         function getResumoPorEstado(estado) {
             var url = avasusService.getUrl('widesus_dashboard', '&estado=' + estado);
--- www/js/app/grafico-cursos/grafico-cursos.directive.js
+++ www/js/app/grafico-cursos/grafico-cursos.directive.js
@@ -11,6 +11,8 @@
             restrict: 'EA',
             templateUrl: 'js/app/grafico-cursos/grafico-cursos.html',
             scope: {
+                filtro: '=',
+                atualizacao: '='
             },
             link: linkFunc,
             controller: Controller,
@@ -21,135 +23,122 @@
         return directive;
 
         function linkFunc(scope, el, attr, ctrl) {
+            scope.$watch('vm.filtro', function (a) {
+                ctrl.activate();
+            });
 
+            scope.$watch('vm.atualizacao', function (a) {
+                ctrl.activate();
+            });
         }
     }
 
-    Controller.$inject = ['highchartsNG', 'avasusService', '$http', '$scope'];
+    Controller.$inject = ['highchartsNG', 'cursoService', '$scope'];
 
     /* @ngInject */
-    function Controller(highchartsNG, avasusService, $http, $scope) {
+    function Controller(highchartsNG, cursoService, $scope) {
         var vm = this;
-
-        activate();
 
         vm.carregando = true;
 
-        function activate() {
-            // highchartsNG.ready(function(){
-                vm.config = {
+        vm.activate = function() {
+            vm.carregando = true;
 
-                    options: {
-                        //This is the Main Highcharts chart config. Any Highchart options are valid here.
-                        //will be overriden by values specified below.
-                        chart: {
-                            type: 'pie'
-                        },
-                        tooltip: {
-                            pointFormat: 'Inscritos: {point.y} ({point.percentage:.1f}%)',
-                            style: {
-                                padding: 10
-                            }//,
-                            // useHTML: true,
-                            // formatter: function() {
-                            //     return "<div style='white-space:normal; min-width:100px; max-width:200px'>" +
-                            //         "<b>" + this.point.name + "</b><br>" +
-                            //         "Acessos: " + this.y + " (" + Highcharts.numberFormat(this.percentage, 2) + "%)</div>";
-                            // }
-                        },
-                        plotOptions: {
-                            pie: {
-                                // allowPointSelect: true,
-                                dataLabels: {
-                                    enabled: false
-                                }
-                            }
-                        }
-                    },
-                    //The below properties are watched separately for changes.
+            vm.config = getConfigMapa();
 
-                    //Series object (optional) - a list of series using normal Highcharts series options.
-                    series: [{ data: [] }],
-                    // series: [{
-                    //     data: [{
-                    //         name: 'Estimulação Precoce',
-                    //         y: 30
-                    //     }, {
-                    //         name: 'Combate ao Aedes Aegypti',
-                    //         y: 22
-                    //     }, {
-                    //         name: 'Oxigenoterapia',
-                    //         y: 12
-                    //     }, {
-                    //         name: 'Teste',
-                    //         y: 8
-                    //     }, {
-                    //         name: 'Outros',
-                    //         y: 7
-                    //     }]
-                    // }],
-                    //Title configuration (optional)
-                    title: {
-                        text: null
-                    },
-                    //Boolean to control showing loading status on chart (optional)
-                    //Could be a string if you want to show specific loading text.
-                    loading: false,
-                    //Configuration for the xAxis (optional). Currently only one x axis can be dynamically controlled.
-                    //properties currentMin and currentMax provided 2-way binding to the chart's maximum and minimum
-                    xAxis: {
-                        currentMin: 0,
-                        currentMax: 20,
-                        title: {text: 'values'}
-                    },
-                    //Whether to use Highstocks instead of Highcharts (optional). Defaults to false.
-                    useHighStocks: false,
-                    //size (optional) if left out the chart will default to size of the div or something sensible.
-                    size: {
-                        width: 300,
-                        height: 300
-                    },
-                    //function (optional)
-                    func: function (chart) {
-                    //setup some logic for the chart
-                    }
-                };
-            // }, vm);
-
-            var url = avasusService.getUrl('widesus_dashboard_curso');
-            $http.get(url).then(
+            cursoService.getDetalhes(vm.filtro).then(
                 function (resultado) {
-                    var cursos = resultado.data.map(function (curso) {
+                    var cursos = resultado.map(function (curso) {
                         return {
                             name: curso.curso,
-                            y: curso.inscritos
+                            y: curso.inscritos,
+                            acessos: curso.acessos,
+                            certificados: curso.certificados
                         };
                     });
 
-                    var cursosOrdenados = cursos.sort(function (a, b) {
-                        return b.y - a.y;
-                    });
-
-                    var topCursos = cursosOrdenados;
-                    var limite = 10;
-                    if (cursosOrdenados.length > limite) {
-                        topCursos = cursosOrdenados.slice(0, limite - 1);
-                        topCursos.push({
-                            name: 'Outros',
-                            y: cursosOrdenados.slice(limite - 1).reduce(
-                                function (total, curso) {
-                                    return total + curso.y;
-                                },
-                                0
-                            )
-                        });
-                    }
-
-                    vm.config.series[0].data = topCursos;
-
+                    vm.config.series[0].data = getTop(cursos, 10);
+                    vm.carregando = false;
+                },
+                function(erro) {
+                    vm.erro = erro;
                     vm.carregando = false;
                 }
             );
+        };
+
+        function getConfigMapa() {
+            return {
+                options: {
+                    chart: {
+                        type: 'pie'
+                    },
+                    tooltip: {
+                        style: {
+                            padding: 10
+                        },
+                        useHTML: true,
+                        formatter: function() {
+                            return "<div>" +
+                                "<strong>" + this.point.name + "</strong><br>" +
+                                "Inscritos: " + Highcharts.numberFormat(this.y, 0) +
+                                " (" + Highcharts.numberFormat(this.percentage, 1) + "%)<br>" +
+                                "Certificados: " + Highcharts.numberFormat(this.point.certificados, 0) + "<br>" +
+                                "Acessos: " + Highcharts.numberFormat(this.point.acessos, 0) + "</div>";
+                        }
+                    },
+                    plotOptions: {
+                        pie: {
+                            dataLabels: {
+                                enabled: false
+                            }
+                        }
+                    }
+                },
+                series: [{ data: [] }],
+                title: {
+                    text: null
+                },
+                size: {
+                    width: 300,
+                    height: 300
+                }
+            };
+        }
+
+        function getTop(cursos, limite) {
+            var cursosOrdenados = cursos.sort(function (a, b) {
+                return b.y - a.y;
+            });
+
+            var topCursos = cursosOrdenados;
+
+            if (cursosOrdenados.length > limite) {
+                topCursos = cursosOrdenados.slice(0, limite - 1);
+                topCursos.push({
+                    name: 'Outros',
+                    y: cursosOrdenados.slice(limite - 1).reduce(
+                        function (total, curso) {
+                            return total + curso.y;
+                        },
+                        0
+                    ),
+                    acessos: cursosOrdenados.slice(limite - 1).reduce(
+                        function (total, curso) {
+                            return total + curso.acessos;
+                        },
+                        0
+                    ),
+                    certificados: cursosOrdenados.slice(limite - 1).reduce(
+                        function (total, curso) {
+                            return total + curso.certificados;
+                        },
+                        0
+                    )
+                });
+            }
+
+            return topCursos;
         }
     }
 })();
