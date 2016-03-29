d32c659c5d04fb60314fbfaed96c9b1d6e79cc58
Mike Job <mikejobrn@gmail.com>
2016-03-29 17:45:16 -0300

    Adiciona localStorageService

---
+++ www/js/app/local-storage/local-storage.service.js
@@ -0,0 +1,49 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('AvasusGestor')
+        .factory('localStorageService', localStorageService);
+
+    localStorageService.$inject = ['$window'];
+
+    /* @ngInject */
+    function localStorageService($window) {
+        var service = {
+            set: set,
+            get: get,
+            setObject: setObject,
+            getObject: getObject,
+            remove: remove,
+            clear: clear
+        };
+
+        return service;
+
+        ///////////////
+
+        function set(key, value) {
+            $window.localStorage[key] = value;
+        }
+
+        function get(key, defaultValue) {
+            return $window.localStorage[key] || defaultValue;
+        }
+
+        function setObject(key, value) {
+            $window.localStorage[key] = JSON.stringify(value);
+        }
+
+        function getObject(key) {
+            return JSON.parse($window.localStorage[key] || '{}');
+        }
+
+        function remove(key) {
+            $window.localStorage.removeItem(key);
+        }
+
+        function clear() {
+            $window.localStorage.clear();
+        }
+    }
+})();
